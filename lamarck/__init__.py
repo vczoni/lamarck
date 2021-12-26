d9a09d030c4ccd5afc4407bae85ed913f4526209
Victor Cunha Zoni <vczoni@gmail.com>
2021-12-26 21:50:58 +0000

    Added __version__ to the main __init__.

--- lamarck/__init__.py
+++ lamarck/__init__.py
@@ -1,9 +1,11 @@
 from lamarck.population import BlueprintBuilder, Blueprint
 from lamarck.optimizer import Optimizer
 from lamarck.explorer import DataExplorer, HistoryExplorer, ParetoExplorer
+from lamarck.version import __version__
 
 
 __all__ = [
+    '__version__',
     'BlueprintBuilder', 'Blueprint', 'Optimizer',
     'DataExplorer', 'HistoryExplorer', 'ParetoExplorer'
 ]
